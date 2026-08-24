6c05eacf9334c3345ed1631f0c69a6d9b813890c
mateusheitor <mateusmendes@blueicontabol.com.br>
2026-08-24 11:18:13 -0300

    refactor: replace lightbulb emoji with SVG icon

--- js/svg.js
+++ js/svg.js
@@ -22,6 +22,8 @@ export const svgEmployeeUser = `<svg xmlns="http://www.w3.org/2000/svg" width="1
 
 export const svgSave = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-save-check-icon lucide-save-check"><path d="M12.5 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h10.2a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4v4.35"/><path d="m16 19 2 2 4-4"/><path d="M17 15.13V14a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"/><path d="M7 3v4a1 1 0 0 0 1 1h7"/></svg>`;
 
+export const svgLightbulb = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lightbulb-icon lucide-lightbulb"><path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5"/><path d="M9 18h6"/><path d="M10 22h4"/></svg>`;
+
 // Helper function to insert SVGs into DOM elements
 export function insertSVGs() {
   document.querySelectorAll('[data-icon]').forEach(el => {
@@ -41,6 +43,7 @@ export function insertSVGs() {
       case 'user-shield': svgString = svgUserShield; break;
       case 'employee-user': svgString = svgEmployeeUser; break;
       case 'save': svgString = svgSave; break;
+      case 'lightbulb': svgString = svgLightbulb; break;
     }
     
     if (svgString && !el.innerHTML.includes('<svg')) {
